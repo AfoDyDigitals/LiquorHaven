0a4c6c3596657538f5b82ac40d8870ad7fa441f9
Justice-Udehegbunam <justicefavour491@gmail.com>
2023-12-17 16:41:29 +0100

    Gotten my assets files and set up

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section className="">
+      <div className="font-rubik font-bold text-slate-gray">
+        <p>Frosty Elegance Extravaganza</p>
+      </div>
+    </section>
+  );
+};
---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+/* Hero assets */
+import hero_dot from "./Dot.png";
+import carousel_image1 from "./unsplash_aK6WGqxyHFw (1).png";
+
+/* Range assets */
+import elipse1 from "./srcassetsEllipse 1 (1).png";
+import elipse2 from "./srcassetsEllipse 1.png";
+import arrow_left from "./srcassetsarrow_forward_ios_FILL0_wght400_GRAD0_opsz24 (1) 1 (1).png";
+import arrow_right from "./srcassetsarrow_forward_ios_FILL0_wght400_GRAD0_opsz24 (1) 1.png";
+
+import wine from "./unsplash_W4Gwb-xz_fA.png";
+import beer from "./unsplash_Gla6keSyoU0.png";
+import cocktail from "./unsplash_F7HkGy4U0fo.png";
+import spirit from "./unsplash_nkUtcTb7SOg.png";
+import tequila from "./unsplash_7tOObrZ8x-o.png";
+
+/* Trending assets */
+import valentino_finest from "./unsplash_B2PMO6-Ap7w.png";
+import starred from "./srcassetsstar_FILL1_wght400_GRAD0_opsz24 (1) 1.png";
+import not_starred from "./srcassetsstar_FILL0_wght400_GRAD0_opsz24 1.png";
