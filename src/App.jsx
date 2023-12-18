fbf68f8e5945fb6c47678141382d30fffab45b3e
Miracle8221 <anagwumiracle098@gmail.com>
2023-12-18 15:14:11 +0100

    setting the footer header (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import './App.css'
+import Footer from './Components/Footer'
 
 function App() {
-
   return (
     <>
-  
+    <Footer />
     </>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
     <div>
-        
+        <div className='flex justify-center items-center flex-col mt-[98px]'>
+            <h2 className='text-[39px] font-bold text-[#1E1E1E] font-rubik leading-[46.8px]'>Get Exclusive Offers & Rewards</h2>
+            <p className='text-[20px] font-normal font-rubik leading-[24px]'>Sign up for our members portal to receive some exclusive offer & rewards.It’s easy and free!</p>
+            <form action="">
+            <input type="text" placeholder="Your Email Address" />
+            <button className='footerButton'>sign in</button>
+            </form>
+        </div>
+
     </div>
   )
 }
