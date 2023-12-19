5f56c836e250a4a208d8e04dc7384ce59376914d
AfoDy <afodydigitals@gmail.com>
2023-12-18 21:45:38 -0800

    complete header and breadcrumb components for NavBar #3

--- src/Components/Breadcrumb.jsx
+++ src/Components/Breadcrumb.jsx
@@ -1,29 +1,85 @@
-import React from 'react'
+import React from "react";
 
 function Breadcrumb() {
   return (
-    <div className='flex justify-center items-center h-[108px] bg-[#A22634] mt-[60px] text-white justify-evenly lg:'>
-        <div className='flex gap-[10px] justify-center items-center '>
-            <img src='../src/assets/call_FILL1_wght400_GRAD0_opsz24 1.svg' />
-            <div className='text-[20px] font-semibold'>Hotline:</div>
-            <div className='text-[20px] font-normal'>+234 80197867</div>
-        </div>
-
-        <div className='flex gap-[50px]'>
-            <div>Home</div>
-            <div className='flex gap-[5px] justify-center items-center'>
-                <div>Shop</div>
-                <img src='../src/assets/Dropdown white.svg' />
-            </div>
-            <div>About Us</div>
-            <div>Contact Us</div>
-        </div>
-
-        <button className='flex justify-center items-center border border-[#E7E3E4] rounded-[8px] p-[20px] h-[46px] bg-transparent'>
-            Sign in
+    <>
+    {/* searchbar */}
+      <div className="flex items-center justify-center justify-between border-solid border border-[#9E9496] w-[343px] h-[35px] rounded-[4px] mx-[16px] mt-[16px] md:hidden lg:hidden">
+        <input
+          className="font-normal focus:outline-none focus:ring-1 focus:ring-[#E66B66] w-[343px] h-[16px] text-[13px] p-[15px] "
+          type="text"
+          placeholder="Search over 3,000 quality drinks..."
+        />
+        <button className="flex justify-center items-center bg-[#A22634] w-[60px] h-[35px] rounded-l">
+          <img
+            className=" "
+            src="../src/assets/search_FILL0_wght400_GRAD0_opsz24 (1) 1.svg"
+          />
         </button>
-    </div>
-  )
+      </div>
+
+    {/* breadcrumb */}
+      <div className=" flex justify-center items-center bg-[#A22634] text-white justify-evenly h-[60px] mt-[22px] md:h-[60px] mt-[40px] lg:h-[108px] mt-[60px]">
+        <div className="flex gap-[10px] justify-center items-center md:ml-[32px]">
+          <img
+            className="w-[16px] md:w-[24px] lg:w-[32px]"
+            src="../src/assets/call_FILL1_wght400_GRAD0_opsz24 1.svg"
+          />
+          <div className="text-[13px] font-semibold md:text-[16px] lg:text-[20px]">
+            Hotline:
+          </div>
+          <div className="text-[13px] md:text-[16px] lg:text-[20px] font-normal">
+            +234 80197867
+          </div>
+        </div>
+
+        <div className="sm:hidden md:hidden lg:flex gap-[50px]">
+          <div>Home</div>
+          <div className="flex gap-[5px] justify-center items-center">
+            <div>Shop</div>
+            <img src="../src/assets/Dropdown white.svg" />
+          </div>
+          <div>About Us</div>
+          <div>Contact Us</div>
+        </div>
+
+        {/* currency & icons */}
+        <div className="flex justify-center items-center gap-[10px] h-[32px] md:gap-[15px] lg:hidden">
+          <div className="flex justify-center items-center gap-[5px] ml-[50px] md:ml-[120px]">
+            <div className="text-[10px] font-normal md:text-[13px]">USD</div>
+            <img
+              className="w-[6px] h-[10px]"
+              src="../src/assets/Dropdown white.svg"
+            />
+          </div>
+
+          <div className="flex">
+            <img
+              className="w-[16px] h-[16px] md:w-[18px] h-[18px]"
+              src="../src/assets/favorite_FILL0_white.svg"
+            />
+            <div className="flex text-black justify-center items-center text-[8px] font-normal bg-[#E66B66]  h-[10px] w-[4px] p-1 rounded-full lg:text-[10px] h-[12.5px] w-[4px]">
+              1
+            </div>
+          </div>
+
+          <div className="flex">
+            <img
+              className="w-[16px] h-[16px] md:w-[18px] h-[18px]"
+              src="../src/assets/shopping_cart_white.svg"
+            />
+            <div className="flex text-black justify-center items-center text-[8px] font-normal bg-[#E66B66]  h-[10px] w-[4px] p-1 rounded-full lg:text-[10px] h-[12.5px] w-[4px]">
+              1
+            </div>
+          </div>
+        </div>
+
+        <button className="hidden md:flex justify-center items-center p-0 w-[66px] h-[32px] border border-[#E7E3E4] rounded-[5px] bg-transparent ml-[40px] mr-[32px] lg:rounded-[8px] w-[93px] h-[46px] ">
+          <div className="md:text-[13px] p-[10px] lg:text-[16px] p-[20px]">Sign in</div>
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Breadcrumb
+export default Breadcrumb;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,37 +2,50 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="flex items-center justify-center justify-between">
-      <img className="w-[226px] h-[50px]" src="../src/assets/LiquorHAVEN-LOGO.png" alt="LiquorHaven Logo" />
+    <div className="flex justify-center items-center  justify-between mx-[16px] md:flex justify-center items-center  justify-between mx-[32px] lg:mx-[45px]">
+      <img className="w-[70px] h-[24px] md:w-[146px] lg:w-[226px]" src="../src/assets/LiquorHAVEN LOGO_.svg" alt="LiquorHaven Logo" />
 
-      <div className="flex w-[877px] h-[61px] rounded-[5px] justify-between items-center border-solid border border-[#9E9496]">
+      {/* searchbar */}
+      <div className="hidden md:flex justify-center items-center w-[381px] h-[46px] rounded-[4px] border border-[#9E9496] lg:w-[877px] h-[62px] flex justify-center items-center justify-between rounded-[4px]">
         <input
-          className="flex w-[800px] h-[16px]text-[13px] p-[15px] font-normal focus:outline-none focus:ring-1 focus:ring-[#E66B66]"
+          className="font-normal focus:outline-none focus:ring-1 focus:ring-[#E66B66] w-[343px]  md:w-[381px] lg:w-[600px] h-[16px] text-[13px] p-[15px] "
           type="text"
           placeholder="Search over 3,000 quality drinks..."
         />
-        <button className="flex justify-center items-center bg-[#A22634] w-[125px] h-[62px]">
+        <button className="flex justify-center items-center bg-[#A22634]  rounded-l md:w-[74px] h-[46px] lg:w-[125px] h-[62px] ">
           <img
-            className=""
+            className=" "
             src="../src/assets/search_FILL0_wght400_GRAD0_opsz24 (1) 1.svg"
           />
         </button>
       </div>
+      <img className=" lg:hidden" src="../src/assets/menuBar.svg" />
 
-      <div className="flex gap-[45px] w-[260px] h-[32px]">
+      {/* currency and icons */}
+      <div className="sm:hidden md:hidden lg:flex gap-[45px] w-[260px] h-[32px]">
         <div className="flex justify-center items-center gap-[5px] ml-[64.59px]">
           <div className="text-[16px] font-normal">USD</div>
           <img className="w-[8px] h-[14px]" src="../src/assets/Dropdown.svg" />
         </div>
 
         <div className="flex">
-          <img className="w-[32px] h-[32px]" src="../src/assets/favorite_FILL0_wght400_GRAD0_opsz24 1.svg" />
-          <div className="text-[10px] font-normal bg-[#E66B66] rounded-[100%] h-[12.5px] w-[5px] p-1">1</div>
+          <img
+            className="w-[32px] h-[32px]"
+            src="../src/assets/favorite_FILL0_wght400_GRAD0_opsz24 1.svg"
+          />
+          <div className="flex justify-center items-center text-[10px] font-normal bg-[#E66B66]  h-[12.5px] w-[5px] p-2 rounded-full">
+            1
+          </div>
         </div>
 
         <div className="flex">
-          <img className="w-[32px] h-[32px]" src="../src/assets/shopping_cart_FILL0_wght400_GRAD0_opsz24 1.svg" />
-          <div className="text-[10px] font-normal flex bg-[#E66B66] rounded-[100%] h-[12.5px] w-[5px] p-1">1</div>
+          <img
+            className="w-[32px] h-[32px]"
+            src="../src/assets/shopping_cart_FILL0_wght400_GRAD0_opsz24 1.svg"
+          />
+          <div className="flex justify-center items-center text-[10px] font-normal flex bg-[#E66B66]  h-[12.5px] w-[5px] p-2 rounded-full">
+            1
+          </div>
         </div>
       </div>
     </div>
