da3660468a64d95ddb9263828cd781d735af4c89
Miracle8221 <anagwumiracle098@gmail.com>
2023-12-26 00:42:31 +0100

    finish footer component (#8)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      // 'sm': '375px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '744px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1200px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
