726260b9f6808cd23d9d3ba2f2d554b7b884b1f5
joeyfav <joefav161@gmail.com>
2024-01-03 06:46:09 +0100

    updating code

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -1,9 +1,19 @@
+import React from 'react'
+
 function Section4() {
+  
     return (
       <>
+        <div classname="bg-gray-600 text-3x1 font-bold item-justify-center">hello world how are u</div>
+        
+          <div classname="mt-1 ml-6 w-[680px] h-[527px] justify-center"> why are my having issues</div>
+
+
+
+
+
 
       </>
     );
-  }
-  export default Section4;
-  
+}
+  export default Section4
