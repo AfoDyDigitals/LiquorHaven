c85f82fe8b2ff3ce31cd257aa2f7bd2f9377d7ce
fabiyis <141014211+fabiyis@users.noreply.github.com>
2024-01-03 03:23:14 -0800

    update on cart.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from 'react'
 import './App.css'
-import NavBar from './Components/NavBar';
-import Section3 from './Components/Section3';
 import Cart from './Pages/Cart';
-
 import './App.css';
 
 function App() {
-  
   return (
 
    <>
-   <NavBar /> 
-   <Section3 />
    <Cart />
    </>
   )
