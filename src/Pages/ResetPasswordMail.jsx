d4da5190a0aa6e36501853fa60ccf105e5439b7c
AfoDy <afodydigitals@gmail.com>
2024-01-04 15:32:54 -0800

    complete ResetPassWord via Email page #25

--- src/Pages/ResetPasswordMail.jsx
+++ src/Pages/ResetPasswordMail.jsx
@@ -6,26 +6,26 @@ function ResetPasswordMail() {
   return (
     <>
       <NavBar />
-      <div className="w-[100vw] h-full bg-[#FDF0F0]">
-        <div className="flex flex-col justify-center items-center font-rubik bg-white shadow-lg w-[324px] md:w-[500px] lg:w-[1000px]">
+      <div className="w-[100vw] h-full bg-[#FDF0F0] flex flex-col justify-center items-center  md:">
+        <div className="flex flex-col justify-center items-center font-rubik bg-white shadow-lg w-[324px] mt-[60px] md:w-[500px] mt-[83px] lg:w-[1000px] h-[800px]">
           <img
             className="mt-[32px] md:mt-[58px]"
             src="./src/assets/ProfileIcon.svg"
           />
           <div className="text-[20px] my-[13px] font-bold md:text-[25px] my-[15px] lg:text-[39px]">Reset your password</div>
-          <div>We will send you an email to reset your password</div>
+          <div className="text-[13px] font-normal lg:text-[20px]">We will send you an email to reset your password</div>
           <input
-            className="bg-[#F7F6F6] w-[252px] rounded-[4px] mt-[48px] mb-[51px] text-[13px] font-normal py-[21px] pl-[13px]  md:text-[16px] pl-[21px] w-[398px] lg:text-[20px] mt-[90px] pl-[42px] w-[797px]"
+            className="bg-[#F7F6F6] w-[252px] rounded-[4px] mt-[48px] mb-[51px] text-sm font-normal py-[21px] pl-[13px]  md:w-[398px] text-base pl-[21px]  lg:w-[797px] text-[20px] mt-[90px] pl-[42px] "
             type=" "
             placeholder="Email address"
           />
           <SubmitButton />
-          <div className="text-[20px] mt-[30px] font-normal underline underline-offset-8  ">
+          <div className="text-[13px] mt-[30px] font-normal underline underline-offset-6 mb-30px md:text-[16px] lg:text-[20px] mb-[240px]">
             Cancel
           </div>
         </div>
 
-        <div className="flex justify-center items-center gap-[40px] md:gap-[60px] lg:gap-[316px]">
+        <div className="flex text-[12px] font-normal gap-[30px] justify-center items-center  mb-[40px] mt-[269px] md:mt-[423px] gap-[60px]  lg:gap-[316px] ">
           <div>All rights reserved © 2024 LiquorHaven</div>
           <div>Privacy | Terms</div>
         </div>
