d6e42f4dda37c4b72009db81754f85b5d5b6314f
Miracle8221 <anagwumiracle098@gmail.com>
2024-01-05 03:03:57 +0100

    adding some functionality for the password

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import  { useState } from 'react';
 import img from '../assets/account icon.png'
 import line from '../assets/line.png'
 import path from '../assets/path .png'
@@ -6,59 +7,76 @@ import facebook from '../assets/facebook2.png'
 import twitter from '../assets/twitter2.png'
 
 function Account() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <div className='bg-[#F9F2F3] w-[100%] h-[93.75rem]'>
-        <div className='w-[1000px] h-[1127px] bg-white mx-auto border border-solid border-[#FDF0F0] shadow-md'>
-        <div className='flex flex-col gap-[15px] items-center'>
-            <img src={img} alt="image description" className='w-[64px] h-[64px]'  />
-            <h1 className='text-[39px] font-bold leading-[46.8px]'>Create your account</h1>
-            <p className='text-20px font-bold leading-[24px]'>These information will help us complete your request</p>
+        <div className='lg:w-[1000px] lg:h-[1127px] md:w-[500px] md:h-[800px] w-[300px] h-[800px] bg-white mx-auto border border-solid border-[#FDF0F0] shadow-md '>
+        <div className='flex flex-col lg:gap-[15px] gap-[9px] items-center'>
+            <img src={img} alt="image description" className='lg:w-[64px] lg:h-[64px] w-[60px] h-[59px]'  />
+            <h1 className='lg:text-[39px] md:text-[32px] text-[24px] font-bold lg:leading-[46.8px] md:leading-[38px] leading-[28px]'>Create your account</h1>
+            <p className='font-rubik lg:text-20px md:text-[14px] text-[10px] lg:font-bold font-normal lg:leading-[24px] leading-[12px]'>These information will help us complete your request</p>
         </div>
-        <div>
-            <div className='w-[807px] h-[99px] flex flex-col justify-center gap-[10px]'>
-                <p className='font-rubik text-[#000] text-[20px] font-medium leading-6'>First name</p>
-                <input type="text" placeholder='Enter First name' className='placeholder:text-[#847B7D] placeholder:text-[16px] placeholder:pl-[21px] rounded bg-[#F7F6F6] h-[65px]'/>
+        <div className='flex flex-col justify-center items-center'>
+            <div className='lg:w-[807px] lg:h-[99px] md:w-[398px] md:h-[65px] w-[258px]  flex flex-col justify-center lg:gap-[10px] lg:mt-[40px] mt-[10px]'>
+                <p className='font-rubik text-[#000] lg:text-[20px] md:text-[16px] text-[13px] lg:font-medium font-bold lg:leading-6 leading-[15px]'>First name</p>
+                <input type="text" placeholder='Enter First name' className='placeholder:text-[#847B7D] placeholder:text-[13px]  px-4 rounded bg-[#F7F6F6] lg:h-[65px] h-[42px]'/>
             </div>
-            <div className='w-[807px] h-[99px] flex flex-col justify-center gap-[10px]'>
-                <p className='font-rubik text-[#000] text-[20px] font-medium leading-6'>Last name</p>
-                <input type="text" placeholder='Enter Last name' className='placeholder:text-[#847B7D] placeholder:text-[16px] placeholder:pl-[21px] rounded bg-[#F7F6F6] h-[65px]'/>
+            <div className='lg:w-[807px] lg:h-[99px] md:w-[398px] md:h-[65px] w-[258px] mt-[30px] flex flex-col justify-center gap-[10px]'>
+                <p className='font-rubik text-[#000] lg:text-[20px] text-[13px] lg:font-medium font-bold lg:leading-6 leading-[15px]'>Last name</p>
+                <input type="text" placeholder='Enter Last name' className='placeholder:text-[#847B7D] placeholder:text-[13px]  px-4 rounded bg-[#F7F6F6] lg:h-[65px] h-[42px]'/>
             </div>
-            <div className='w-[807px] h-[99px] flex flex-col justify-center gap-[10px]'>
-                <p className='font-rubik text-[#000] text-[20px] font-medium leading-6'>Email address</p>
-                <input type="text" placeholder='Enter Email address' className='placeholder:text-[#847B7D] placeholder:text-[16px] placeholder:pl-[21px] rounded bg-[#F7F6F6] h-[65px]'/>
+            <div className='lg:w-[807px] lg:h-[99px] md:w-[398px] md:h-[65px] w-[258px]  mt-[30px] flex flex-col justify-center gap-[10px]'>
+                <p className='font-rubik text-[#000] lg:text-[20px] text-[13px] lg:font-medium font-bold lg:leading-6 leading-[15px]'>Email address</p>
+                <input type="email" placeholder='Enter Email address' className='placeholder:text-[#847B7D] placeholder:text-[13px] px-4 rounded bg-[#F7F6F6] h-[42px]'/>
             </div>
-            <div className='w-[807px] h-[99px] flex flex-col justify-center gap-[10px]'>
-                <p className='font-rubik text-[#000] text-[20px] font-medium leading-6'>Phone number</p>
-                <input type="text" placeholder='Enter Phone number' className='placeholder:text-[#847B7D] placeholder:text-[16px] placeholder:pl-[21px] rounded bg-[#F7F6F6] h-[65px]'/>
+            <div className='lg:w-[807px] lg:h-[99px] md:w-[398px] md:h-[65px] w-[258px]  mt-[30px] flex flex-col justify-center gap-[10px]'>
+                <p className='font-rubik text-[#000] lg:text-[20px] text-[13px] lg:font-medium font-bold lg:leading-6 leading-[15px]'>Phone number</p>
+                <input type="tel" placeholder='Enter Phone number' className='placeholder:text-[#847B7D] placeholder:text-[13px] px-4 rounded bg-[#F7F6F6] h-[42px]'/>
             </div>
-            <div className='w-[807px] h-[99px] flex flex-col justify-center gap-[10px]'>
-                <p className='font-rubik text-[#000] text-[20px] font-medium leading-6'>Password</p>
-                <input type="text" placeholder='Enter password' className='placeholder:text-[#847B7D] placeholder:text-[16px] placeholder:pl-[21px] rounded bg-[#F7F6F6] h-[65px]'/>
-            </div>
-        </div>
-        <div className='flex gap-5'>
-            <p className='text-[16px] font-normal'>Already have an account?</p>
-            <button className='text-[#E66B66] text-[20px] font-medium'>Sign in</button>
-            <div>
-                <button className='w-[140px] p-3 rounded-lg border border-solid border-[#E66B66] bg-[#E66B66] font-medium text-[#fff] leading-6 text-[16px]'>Create Account</button>
+            <div className='lg:w-[807px] lg:h-[99px] md:w-[398px] md:h-[65px] w-[258px]  mt-[30px] flex flex-col justify-center lg:gap-[10px] gap-[7px]'>
+                <p className='font-rubik text-[#000] lg:text-[20px] text-[13px] lg:font-medium font-bold lg:leading-6 leading-[15px]'>Password</p>
+                <div className='relative'>
+                <input type={passwordVisible ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} id='password' placeholder='Enter password' className='placeholder:text-[#847B7D] placeholder:text-[13px] px-4 rounded bg-[#F7F6F6] h-[42px] w-full'/>
+                <span className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm cursor-pointer' onClick={togglePasswordVisibility}>{passwordVisible ? 'Hide' : 'Show'}</span>
+                </div>
+                
             </div>
         </div>
-        <div>
-            <div className='flex gap-[40px]'>
-                <img src={line} alt="image description"  />
-                <p className='text-[#7C7C7C] text-[20px] font-normal'>Or Sign Up with</p>
-                <img src={line} alt="image description" />
+        <div className='flex  mt-[50px] justify-around items-center flex-col-reverse  '>
+            <div className='flex gap-5 mt-[10px] items-center'>
+            <p className='lg:text-[16px] text-[11px] font-normal leading-[13px]'>Already have an account?</p>
+            <button className='text-[#E66B66] lg:text-[20px] font-medium'>Sign in</button>
             </div>
             <div>
+                <button className='lg:w-[140px] lg:p-3 p-2 rounded-lg border border-solid border-[#E66B66] bg-[#E66B66] font-medium text-[#fff] leading-6 lg:text-[16px] text-[13px]'>Create Account</button>
+            </div>
+        </div>
+        <div>
+            <div className='flex lg:gap-[40px] gap-[10px] items-center justify-center lg:mt-[35px] mt-[10px]'>
+                <img src={line} alt="image description" className='lg:w-[243px] lg:h-[13px] w-[63px]'/>
+                <p className='text-[#7C7C7C] lg:text-[20px] text-[12px] font-normal leading-[14px]'>Or Sign Up with</p>
+                <img src={line} alt="image description" className='lg:w-[243px] lg:h-[13px] w-[63px]'/>
+            </div>
+            <div className='flex lg:gap-[57px] gap-[20px] items-center justify-center lg:mt-[21px] mt-[10px]'>
                 <img src={path} alt="" />
                 <img src={facebook} alt="" />
                 <img src={twitter} alt="" />
             </div>
-            <div>
-                <p>Want to make a quick order?</p>
-                <button>Order as a Guest</button>
+            <div className='flex lg:gap-[15px] gap-[5px] items-center justify-center mt-[15px]'>
+                <p className='text-[12px] font-normal leading-[14px]'>Want to make a quick order?</p>
+                <button className='text-[#E66B66] text-[12px] font-medium leading-[14px]'>Order as a Guest</button>
             </div>
         </div>
+        </div>
+        <div className=' flex justify-center items-center lg:gap-[250px] gap-[68px]'>
+            <p className='lg:text-[14px] text-[10px] font-normal'>All rights reserved © 2024 LiquorHaven</p>
+            <p className='lg:text-[14px] text-[10px] font-normal'>Privacy | Terms</p>
         </div>
 
     </div>
