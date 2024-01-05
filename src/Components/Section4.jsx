b0c64376885a9b94d0a9979cf96bebc1cd778478
joeyfav <joefav161@gmail.com>
2024-01-05 07:48:45 +0100

    adding bg img contents

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -4,12 +4,16 @@ function Section4() {
   
     return (
       <>
+      
+      <div className="bg">
       <div className="mt-20 mx-auto text-2 font-normal leading-none font-[Rubik]">Mix and sip</div>
       <div className="mt-20 mx-auto">Best Cocktails Limited Edition</div>
       <div className="mt-20 mx-auto">Toast to a New Beginings</div>
       <div className="text-white text-base font-medium font-'Rubik' leading-tight">
                 See More
               </div>
+        </div>
+        
 
         <div className="justify-center items-center ">
         <div className="mt-16 mx-auto">
