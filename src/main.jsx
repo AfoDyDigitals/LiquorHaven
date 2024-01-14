426682491b94ad7d2a9be4a7773e4bb3f34dd160
KofoCole <kofodym07@yahoo.com>
2024-01-14 23:06:18 +0100

    Revamped Footer

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,6 @@ import App from './App.jsx';
 import './index.css';
 
 
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
