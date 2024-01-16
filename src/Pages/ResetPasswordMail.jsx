95be13cb50175e09022f59b330659727b651a70a
AfoDy <afodydigitals@gmail.com>
2024-01-16 18:16:16 +0100

    fix resetpassword navigation

--- src/Pages/ResetPasswordMail.jsx
+++ src/Pages/ResetPasswordMail.jsx
@@ -56,9 +56,9 @@ function ResetPasswordMail() {
           />
           
             <p className="text-red-400 text-sm mb-[51px]">{error}</p>
-            <button onClick={submit} className="-mt-[0.6rem]">
+            <Link to="forgotpassword/code" onClick={submit} className="-mt-[0.6rem]">
               <SubmitButton />
-            </button>
+            </Link>
             <div className="text-[13px] mt-[23px] font-normal hover:underline hover:underline-offset-6 mb-30px md:text-[16px] lg:text-[20px] lg:mb-[240px] cursor-pointer ">
               <Link to="/">Cancel</Link>
             
